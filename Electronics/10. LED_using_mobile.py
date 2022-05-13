a0a29a6016d5c3fa2a7df34ee2365a35c9b36942
Preseus-101 <dbdaniels01@gmail.com>
2022-05-13 20:43:47 +0530

    Updated code

--- Electronics/10. LED_using_mobile.py
+++ Electronics/10. LED_using_mobile.py
@@ -3,7 +3,7 @@ import RPi.GPIO as G
 from flask import Flask, render_template, request
 import datetime
 
-app = Flask(__name__)               # create and configure the app
+app = Flask(__name__)           # create and configure the app
 G.setmode(G.BCM)
 G.setwarnings(False)
 
@@ -45,7 +45,9 @@ def action(deviceName, action):
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 80, debug = True)
 
-# To run this code, connect RPi to a wifi network, note down the IP address;
-# then head over to the terminal, change the directory to where this file is saved
-# and type 'sudo python3 filename.py' ; then open up browser on your phone 
-# or on the RPi itself and enter the IP address noted down earlier in the URL box
+# To run this code, connect RPi to a wifi network, note down 
+# the IP address; then head over to the terminal, change the 
+# directory to where this file is saved and type 
+# 'sudo python3 filename.py' ; then open up browser on your 
+# phone or on the RPi itself and enter the IP address noted down earlier
+# in the URL box
--- Electronics/2. LED.py
+++ Electronics/2. LED.py
@@ -1,24 +1,24 @@
-# This is the modified version of the ma'am told us to do,
+# This is the modified version which ma'am told us to do,
 # incase she doesn't require us to write this verion of the program
 # just delete all the lines of code that includes GPIO 6 pin and edit the 
 # print statement to just include-'LED ON' or 'LED OFF'
 
 # LED interfacing with RPi
-import RPi.GPIO as G                        # include the GPIO library
-import time                                 # include the built in time library
+import RPi.GPIO as G            # include the GPIO library
+import time                     # include the built in time library
 
-G.setmode(G.BCM)                            # setting the mode to BCM numbering system
-G.setwarnings(False)                        # supress any warnings
+G.setmode(G.BCM)                # setting the mode to BCM numbering system
+G.setwarnings(False)            # supress any warnings
 
-G.setup(5,G.OUT)                            # configuring both GPIO 5 and GPIO 6 
-G.setup(6,G.OUT)                            # as output port
+G.setup(5,G.OUT)                # configuring both GPIO 5 and GPIO 6 
+G.setup(6,G.OUT)                # as output port
 
-while True:                                 # starting the infinite loop
-    G.output(5,G.HIGH)                      # sending logic 1 to GPIO 5 to turn ON LED
-    G.output(6,G.LOW)                       # sending logic 0 to GPIO 6 to turn OFF LED
+while True:                     # starting the infinite loop
+    G.output(5,G.HIGH)          # sending logic 1 to GPIO 5 to turn ON LED
+    G.output(6,G.LOW)           # sending logic 0 to GPIO 6 to turn OFF LED
     print("LED 1 is ON and LED 2 is OFF")
-    time.sleep(1)                           # creating a delay of 1 second
-    G.output(5,G.LOW)                       # sending logic 0 to GPIO 5 to turn OFF LED
-    G.output(6,G.HIGH)                      # sending logic 1 to GPIO 6 to turn ON LED
+    time.sleep(1)               # creating a delay of 1 second
+    G.output(5,G.LOW)           # sending logic 0 to GPIO 5 to turn OFF LED
+    G.output(6,G.HIGH)          # sending logic 1 to GPIO 6 to turn ON LED
     print("LED 1 is OFF and LED 2 is ON")
-    time.sleep(1)                           # creating a delay of 1 second
+    time.sleep(1)               # creating a delay of 1 second
